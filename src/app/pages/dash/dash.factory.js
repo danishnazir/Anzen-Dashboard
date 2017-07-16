884d5e0f4a581be24275b9c6445514089bc4ad8a
Danish Nazir <dn8034@gmail.com>
2017-07-16 14:16:55 +0500

    Update dash.factory.js

--- src/app/pages/dash/dash.factory.js
+++ src/app/pages/dash/dash.factory.js
@@ -5,13 +5,13 @@ var PostRequests={};
 PostRequests.getCameraUrl=function()
 { 
 var data1={user_id:$sessionStorage.user_id};
-return $http({url: 'http://videoupload.hopto.org:5000/api/Cam_Mac',method: "POST",data: data1});
+return $http({url: 'http://:5000/api/Cam_Mac',method: "POST",data: data1});
 };
 PostRequests.getLogs=function()
 {
 var data={userID:$sessionStorage.user_id};
 return $http({
-        url: 'http://videoupload.hopto.org:5000/api/get_all_log_website',
+        url: 'http://:5000/api/get_all_log_website',
         method: "POST",
         data: data
     });
